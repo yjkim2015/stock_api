6d16303d298af456e9f96d534d54d4f7522dd061
gmarket\ykim14 <yjkim14@gmarket.com>
2024-09-23 20:53:30 +0900

    first commit

---
+++ src/test/java/com/gmarket/stock/StockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmarket.stock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
