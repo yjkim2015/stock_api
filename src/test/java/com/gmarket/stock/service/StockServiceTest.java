3ba4cbf3985a5e15f48709b18e9a7d1e2afb55ba
gmarket\ykim14 <yjkim14@gmarket.com>
2024-09-23 21:28:39 +0900

    재고 로직 문제점

--- src/test/java/com/gmarket/stock/service/StockServiceTest.java
+++ src/test/java/com/gmarket/stock/service/StockServiceTest.java
@@ -8,6 +8,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
@@ -41,6 +45,29 @@ class StockServiceTest {
 
         Stock stock = stockRepository.findById(1L).orElseThrow();
         assertEquals(99, stock.getQuantity());
+    }
+
+    @Test
+    public void 동시에_100개의_요청() throws InterruptedException {
+        int threadCount = 100;
+
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        CountDownLatch countDownLatch = new CountDownLatch(threadCount);
+        for (int i = 0; i < threadCount; i++) {
+            executorService.submit(() -> {
+                try {
+                    stockService.decrease(1L, 1L);
+                }
+                finally {
+                    countDownLatch.countDown();
+                }
+            });
+        }
+
+        countDownLatch.await();
+
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+        assertEquals(0, stock.getQuantity());
     }
 
 
